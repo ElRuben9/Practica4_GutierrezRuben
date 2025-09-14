de4aba28a3654ff494856e9ef004f06eba278d30
ElRuben9 <150877087+ElRuben9@users.noreply.github.com>
2025-09-14 12:02:06 -0700

    sa

---
+++ Practica4_GutierrezRuben/app/src/main/java/com/example/practica4_gutierrezruben/MainActivity2.kt
@@ -0,0 +1,25 @@
+package com.example.practica4_gutierrezruben
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.practica4_gutierrezruben.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMain2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
